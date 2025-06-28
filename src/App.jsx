8b776c7bef9526a4886f716b977bc2ca02b9eaa2
Deepankar Sah <deepankerrocking@gmail.com>
2025-06-28 19:45:09 +0530

    homepage updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route,  } from 'react-router-dom'
 
 
-import Homepage from './Homepage/Homepage'
+import Homepage from './pages/homepage/Homepage'
 import Todo from './pages/TodoApp/Todo'
 
 
